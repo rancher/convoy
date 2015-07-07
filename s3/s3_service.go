e639cbdf3c2de0e3cb8a07a68cfb67d96975b2cf
Sheng Yang <sheng@yasker.org>
2015-07-07 15:02:00 -0700

    s3: Get aws-sdk-go library from aws rather than awslabs
    Congratulations to AWS SDK for Go v0.6.0 Developer Preview Release!

--- s3/s3_service.go
+++ s3/s3_service.go
@@ -2,10 +2,10 @@ package s3
 
 import (
 	"fmt"
-	"github.com/awslabs/aws-sdk-go/aws"
-	"github.com/awslabs/aws-sdk-go/aws/awserr"
-	"github.com/awslabs/aws-sdk-go/aws/awsutil"
-	"github.com/awslabs/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/awsutil"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"io"
 )
 
