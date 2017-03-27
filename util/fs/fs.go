2a0dd9e96f45d47dee55e157e50c5ebffb33ea8d
Jay Taylor <outtatime@gmail.com>
2017-03-27 14:16:44 -0700

    Fixed FS package tests which were inadvertently being skipped in some cases. (#35)
    - Fixed privilege detection in FS package tests which were being skipped when run
      in a container as root but without a `sudo' command available.
    
    - Updated test error messages to ensure they're consistent with the rest of the
      `fs` package's tests.

--- util/fs/fs.go
+++ util/fs/fs.go
@@ -5,19 +5,27 @@ import (
 	"errors"
 	"fmt"
 	"os/exec"
+	"os/user"
 )
 
-var ErrNoFilesystemDetected = errors.New("no filesystem detected")
+var (
+	ErrUnrecognizedFilesystemType = errors.New("unrecognized or unsupported filesystem type")
+	ErrNoFilesystemDetected       = errors.New("no filesystem detected")
+	ErrResizeNotAvailable         = errors.New("resize function not available for the filesystem type of this volume")
+)
 
 func FormatDevice(devicePath string, fsType string) error {
 	switch fsType {
 	case "btrfs", "ext2", "ext3", "ext4", "minix", "xfs":
 	default:
-		return fmt.Errorf("unrecognized or unsupported fs-type: %s", fsType)
+		return ErrUnrecognizedFilesystemType
 	}
-	cmd := exec.Command("sh", "-c", fmt.Sprintf("yes | sudo -n mkfs.%s %s", fsType, devicePath))
+	cmd, err := sudoCmd("sh", "-c", fmt.Sprintf("set -e && yes | mkfs.%v %v", fsType, devicePath))
+	if err != nil {
+		return err
+	}
 	if output, err := cmd.CombinedOutput(); err != nil {
-		return fmt.Errorf("FormatDevice: %s: %s", string(output), err)
+		return fmt.Errorf("FormatDevice: %v: %s", string(output), err)
 	}
 	return nil
 }
@@ -25,7 +33,10 @@ func FormatDevice(devicePath string, fsType string) error {
 // Detect determines the filesystem type for the given device.
 // An empty-string return indicates an unformatted device.
 func Detect(devicePath string) (string, error) {
-	cmd := exec.Command("sudo", "-n", "blkid", "-s", "TYPE", "-o", "value", devicePath)
+	cmd, err := sudoCmd("blkid", "-s", "TYPE", "-o", "value", devicePath)
+	if err != nil {
+		return "", err
+	}
 	output, err := cmd.CombinedOutput()
 	output = bytes.Trim(output, "\r\n \t")
 	if err != nil {
@@ -33,7 +44,7 @@ func Detect(devicePath string) (string, error) {
 			// Then no filesystem detected.
 			return "", ErrNoFilesystemDetected
 		}
-		return "", fmt.Errorf("Detect: %s: %s", string(output), err)
+		return "", fmt.Errorf("Detect: %v: %s", string(output), err)
 	}
 	fsType := string(output)
 	return fsType, nil
@@ -43,11 +54,48 @@ func Detect(devicePath string) (string, error) {
 // resize2fs only runs a resize when it is
 // required on the device; otherwise, it just exits with a code 0 and a message.
 func Resize(devicePath string) error {
-	cmd := exec.Command("sudo", "-n", "resize2fs", "-f", devicePath)
+	fsType, err := Detect(devicePath)
+	if err != nil {
+		return err
+	}
+
+	switch fsType {
+	case "ext2", "ext3", "ext4":
+	default:
+		return ErrResizeNotAvailable
+	}
+
+	cmd, err := sudoCmd("resize2fs", "-f", devicePath)
+	if err != nil {
+		return err
+	}
 	output, err := cmd.CombinedOutput()
 	output = bytes.Trim(output, "\r\n \t")
 	if err != nil {
-		return fmt.Errorf("Resize: %s: %s", devicePath, string(output))
+		return fmt.Errorf("Resize: %v: %v", devicePath, string(output))
 	}
 	return nil
+}
+
+func sudoCmd(name string, args ...string) (*exec.Cmd, error) {
+	prefix, err := sudoCmdPrefix()
+	if err != nil {
+		return nil, err
+	}
+	args = append(prefix, append([]string{name}, args...)...)
+	cmd := exec.Command(args[0], args[1:]...)
+	return cmd, nil
+}
+
+// sudoCmdPrefix accounts for sudo only being necessary when UID != 0 (i.e. when not
+// root).
+func sudoCmdPrefix() ([]string, error) {
+	u, err := user.Current()
+	if err != nil {
+		return nil, err
+	}
+	if u.Uid != "0" {
+		return []string{"sudo", "-n"}, nil
+	}
+	return nil, nil
 }
--- util/fs/fs_test.go
+++ util/fs/fs_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"strings"
 	"testing"
 )
 
@@ -15,14 +16,19 @@ func TestDeviceFormatter(t *testing.T) {
 	if runtime.GOOS != "linux" {
 		t.Skip("TestDeviceFormatter skipped because OS is not 'linux'")
 	}
-	if !hasPasswordlessSudo() {
-		t.Skip("TestDeviceFormatter skipped because password-less sudo is required and not presently available")
+	if !hasRoot() {
+		t.Skip("TestDeviceFormatter skipped because sudo privileges are not currently available")
 	}
 
-	for _, targetFsType := range supportedFsTypes {
+	for _, targetFSType := range supportedFsTypes {
+		if _, err := exec.LookPath(fmt.Sprintf("mkfs.%v", targetFSType)); err != nil { // Verify existence of mkfs.X in $PATH or skip.
+			t.Logf("Skipping formatter test for fsType=%[1]s because 'mkfs.%[1]s' binary was not found in $PATH (err=%v)", targetFSType, err)
+			continue
+		}
+
 		func() {
 			// Create fake device.
-			fakeDevicePath := fmt.Sprintf("/tmp/TestDeviceFormatter-%s.img", targetFsType)
+			fakeDevicePath := fmt.Sprintf("/tmp/TestDeviceFormatter-%v.img", targetFSType)
 			if err := makeFakeDevice(fakeDevicePath); err != nil {
 				t.Fatal(err)
 			}
@@ -36,12 +42,12 @@ func TestDeviceFormatter(t *testing.T) {
 
 			// Unformatted case.
 			if fsType, err := Detect(fakeDevicePath); err == nil {
-				t.Fatalf("Expected non-nil error from Detect for unformatted device, but got fsType=%s err=%+v", fsType, err)
+				t.Fatalf("Expected non-nil error from Detect for unformatted device, but got fsType=%v err=%s", fsType, err)
 			}
 
 			// Format it.
-			if err := FormatDevice(fakeDevicePath, targetFsType); err != nil {
-				t.Fatalf("Unexpected error formatting device=%s: %s", fakeDevicePath, err)
+			if err := FormatDevice(fakeDevicePath, targetFSType); err != nil {
+				t.Fatalf("Unexpected error formatting device=%v with fsType=%v: %s", fakeDevicePath, targetFSType, err)
 			}
 		}()
 	}
@@ -51,80 +57,95 @@ func TestFSDetectorResize(t *testing.T) {
 	if runtime.GOOS != "linux" {
 		t.Skip("TestFSDetectorResize skipped because OS is not 'linux'")
 	}
-	if !hasPasswordlessSudo() {
-		t.Skip("TestFSDetectorResize skipped because password-less sudo is required and not presently available")
+	if !hasRoot() {
+		t.Skip("TestFSDetectorResize skipped because sudo privileges are not currently available")
 	}
 
 	// Non-existent test case.
 	if fsType, err := Detect("/TestFSDetectorResize/foo/bar/baz"); err != ErrNoFilesystemDetected {
-		t.Errorf("Expected ErrNoFilesystemDetected error from Detect, but got result fsType=%s err=%+v", fsType, err)
+		t.Errorf("Expected ErrNoFilesystemDetected error from Detect, but got result fsType=%v err=%s", fsType, err)
 	}
 
 	// Non-existent test case.
-	if err := Resize("/TestFSDetectorResize/foo/bar/baz"); err == nil  {
+	if err := Resize("/TestFSDetectorResize/foo/bar/baz"); err == nil {
 		t.Error("Expected error from Resize, but got nothing")
 	}
 
-	for _, targetFsType := range supportedFsTypes {
+	for _, targetFSType := range supportedFsTypes {
+		if _, err := exec.LookPath(fmt.Sprintf("mkfs.%v", targetFSType)); err != nil { // Verify existence of mkfs.X in $PATH or skip.
+			t.Logf("Skipping resize test for fsType=%[1]s because 'mkfs.%[1]s' binary was not found in $PATH (err=%v)", targetFSType, err)
+			continue
+		}
+
 		func() {
 			// Create fake device.
-			fakeDevicePath := fmt.Sprintf("/tmp/TestFSDetectorResize-%s.img", targetFsType)
+			fakeDevicePath := fmt.Sprintf("/tmp/TestFSDetectorResize-%v.img", targetFSType)
 			if err := makeFakeDevice(fakeDevicePath); err != nil {
-				t.Fatal(err)
+				t.Fatalf("Unexpected error making fake device for fsType=%v: %s", targetFSType, err)
 			}
 
 			// Cleanup and remove it afterwards.
 			defer func() {
 				if err := os.RemoveAll(fakeDevicePath); err != nil {
-					t.Error(err.Error())
+					t.Errorf("Unexpected error cleaning up fake device path=%v: %s", fakeDevicePath, err)
 				}
 			}()
 
 			// Unformatted case.
 			if fsType, err := Detect(fakeDevicePath); err != ErrNoFilesystemDetected {
-				t.Fatalf("Expected ErrNoFilesystemDetected from Detect for unformatted device, but got fsType=%s err=%+v", fsType, err)
+				t.Fatalf("Expected ErrNoFilesystemDetected from Detect() for unformatted device and fsType=%v, but err=%s", fsType, err)
 			}
 
 			// Unformatted case.
 			if err := Resize(fakeDevicePath); err == nil {
-				t.Fatal("Expected error from Resize for unformatted device, but got nothing")
+				t.Fatal("Expected error from Resize() for unformatted device and fsType=%v, but err=%s", targetFSType, err)
 			}
 
 			// Format it.
-			if err := FormatDevice(fakeDevicePath, targetFsType); err != nil {
-				t.Fatalf("Unexpected error formatting device=%s: %s", fakeDevicePath, err)
+			if err := FormatDevice(fakeDevicePath, targetFSType); err != nil {
+				t.Fatalf("Unexpected error formatting device=%v: %s", fakeDevicePath, err)
 			}
 
 			// Formatted case.
 			{
 				fsType, err := Detect(fakeDevicePath)
 				if err != nil {
-					t.Errorf("Unexpected error from Detect: %s", err)
+					t.Errorf("Unexpected error from Detect() for fsType=%v: %s", targetFSType, err)
 				}
-				if expected, actual := targetFsType, fsType; actual != expected {
-					t.Errorf("Wrong result from Detect, expected fsType=%q but actual=%q", expected, actual)
+				if expected, actual := targetFSType, fsType; actual != expected {
+					t.Errorf("Incorrect result from Detect(), expected fsType=%q but actual=%q", expected, actual)
 				}
 
 				err = Resize(fakeDevicePath)
-				if err != nil {
-					t.Errorf("Unexpected error in resize: %s", err)
+				if strings.HasPrefix(targetFSType, "ext") {
+					if err != nil {
+						t.Errorf("Unexpected error from Resize() for fsType=%v: %s", targetFSType, err)
+					}
+				} else if err == nil {
+					t.Errorf("Expected error from Resize() for fsType=%v, but err=%s", targetFSType, err)
 				}
 			}
 		}()
 	}
 }
 
-
 func makeFakeDevice(path string) error {
 	size := 100 // NB: btrfs requires a minimum size of 100MB.
-	if output, err := exec.Command("dd", "if=/dev/zero", fmt.Sprintf("of=%s", path), "bs=1M", fmt.Sprintf("count=%v", size)).CombinedOutput(); err != nil {
-		return fmt.Errorf("fake device setup failed: dd command output=%s err=%s", string(bytes.Trim(output, "\r\n \t")), err)
+	if output, err := exec.Command("dd", "if=/dev/zero", fmt.Sprintf("of=%v", path), "bs=1M", fmt.Sprintf("count=%v", size)).CombinedOutput(); err != nil {
+		return fmt.Errorf("fake device setup failed: dd command output=%v err=%s", string(bytes.Trim(output, "\r\n \t")), err)
 	}
 	return nil
 }
 
-func hasPasswordlessSudo() bool {
-	cmd := exec.Command("sudo", "-n", "true")
+func hasRoot() bool {
+	cmd, err := sudoCmd("true")
+	if err != nil {
+		panic(err)
+	}
+	if len(cmd.Args) == 1 {
+		// Already root.
+		return true
+	}
 	if err := cmd.Run(); err != nil {
 		return false
 	}
